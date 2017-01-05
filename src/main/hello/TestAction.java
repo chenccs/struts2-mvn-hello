4ff74709d54c4bcac9884c35d855b80fa63d7772
Administrator <2416762675@qq.com>
2017-01-05 12:06:51 +0800

    all

---
+++ src/main/hello/TestAction.java
@@ -0,0 +1,24 @@
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * Created by Administrator on 2017/1/5.
+ */
+public class TestAction extends ActionSupport{
+
+    public static final String MESSAGE = "Struts is up and running ......";
+
+    public String execute() throws Exception {
+        setMessage(MESSAGE);
+        return SUCCESS;
+    }
+
+    private String message;
+
+    public void setMessage(String message){
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%@ taglib prefix="s" uri="/struts-tags" %>
+
+
+<html>
+<head>
+    <title>Hello World!</title>
+</head>
+<body>
+<h2><s:property value="message" /></h2>
+<s:debug />
+<h2>${message}</h2>
+<h1>Here</h1>
+
+
+</body>
+</html>
